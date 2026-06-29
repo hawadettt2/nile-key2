011175ac1ec074b9b83186402e5174c508ea007f
hawadettt2 <hawadettt2@gmail.com>
2026-06-29 04:18:46 +0300

    Update __init__.py

---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,19 @@
+from .auth import router
+from .shipping import router
+from .invoice import router
+from .suppliers import router
+from .customers import router
+from .customs import router
+from .resources import router
+from .documents import router
+
+__all__ = [
+    "auth",
+    "shipping",
+    "invoice",
+    "suppliers",
+    "customers",
+    "customs",
+    "resources",
+    "documents",
+]
